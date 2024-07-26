370bf775132c9d1f7ddebd30210d1ed8f4d4b3d6
edward-Han1024 <babyed.han@gmail.com>
2024-07-25 20:20:14 -0700

    fix semicolons main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function sieveOfEratosthenes(n = 100) {
      * Args:
      * - n (int) sieve to n
      */
-    console.log("start")
+    console.log("start");
     let primes1 = [];
     let primes2 = [];
     let primes3 = [];
@@ -42,20 +42,20 @@ function sieveOfEratosthenes(n = 100) {
             return primes5[l-4*n/5-1];
         }
     }
-    console.log("filling array")
+    console.log("filling array");
     primes1 = Array(n/5 + 1).fill(true);  // Assume all numbers in array are prime
-    console.log("1 done")
+    console.log("1 done");
     primes2 = Array(n/5).fill(true);  // Assume all numbers in array are prime
-    console.log("2 done")
+    console.log("2 done");
     primes3 = Array(n/5).fill(true);  // Assume all numbers in array are prime
-    console.log("3 done")
+    console.log("3 done");
     primes4 = Array(n/5).fill(true);  // Assume all numbers in array are prime
-    console.log("4 done")
+    console.log("4 done");
     primes5 = Array(n/5).fill(true);  // Assume all numbers in array are prime
-    console.log("Array filling done!")
+    console.log("Array filling done!");
     primes1[0] = primes1[1] = false;         // Except for 0 and 1
     k = 0;
-    console.log("starting main loop")
+    console.log("starting main loop");
     for (let i = 2; i <= Math.sqrt(n); i++) {
         if (getValue(i)) {
             for (let j = i * i; j <= n; j += i) {
@@ -81,29 +81,29 @@ function sieveOfEratosthenes(n = 100) {
             }
         }
     }
-    console.log("\nmain loop over")
+    console.log("\nmain loop over");
     let primes = [];
-    console.log("final list")
+    console.log("final list");
     primes.push(primes1.reduce((acc, isPrime, index) => {
         if (isPrime) acc.push(index);
         return acc;
     }, []));
-    console.log("1 done")
+    console.log("1 done");
     primes.push(primes2.reduce((acc, isPrime, index) => {
         if (isPrime) acc.push(index + n/5 + 1);
         return acc;
     }, []));
-    console.log("2 done")
+    console.log("2 done");
     primes.push(primes3.reduce((acc, isPrime, index) => {
         if (isPrime) acc.push(index + 2*n/5 + 1);
         return acc;
     }, []));
-    console.log("3 done")
+    console.log("3 done");
     primes.push(primes4.reduce((acc, isPrime, index) => {
         if (isPrime) acc.push(index + 3*n/5 + 1);
         return acc;
     }, []));
-    console.log("4 done")
+    console.log("4 done");
     primes.push(primes5.reduce((acc, isPrime, index) => {
         if (isPrime) acc.push(index + 4*n/5 + 1);
         return acc;
@@ -118,16 +118,16 @@ const readline = require('readline').createInterface({
 readline.question("int ", (iInput) => {
     const i = parseInt(iInput);
     const res = sieveOfEratosthenes(i);
-    console.log("storing files")
+    console.log("storing files");
     fs.writeFileSync("./primes/primes1.txt", JSON.stringify(res[0]));
-    console.log("1 done")
+    console.log("1 done");
     fs.writeFileSync("./primes/primes2.txt", JSON.stringify(res[1]));
-    console.log("2 done")
+    console.log("2 done");
     fs.writeFileSync("./primes/primes3.txt", JSON.stringify(res[2]));
-    console.log("3 done")
+    console.log("3 done");
     fs.writeFileSync("./primes/primes4.txt", JSON.stringify(res[3]));
-    console.log("4 done")
+    console.log("4 done");
     fs.writeFileSync("./primes/primes5.txt", JSON.stringify(res[4]));
-    console.log("all done closing...")
+    console.log("all done closing...");
     readline.close();
 });
