c789f78b0b278b73fa24799517d7e65ae18c9840
edward-Han1024 <babyed.han@gmail.com>
2024-07-25 20:33:37 -0700

    format

--- main.js
+++ main.js
@@ -25,33 +25,33 @@ function sieveOfEratosthenes(n = 100) {
     let primes3 = [];
     let primes4 = [];
     let primes5 = [];
-    function getValue(l){
-        if (l <= n/5){
+    function getValue(l) {
+        if (l <= n / 5) {
             return primes1[l];
         }
-        else if(l <= 2*n/5){
-            return primes2[l-n/5-1];
+        else if (l <= 2 * n / 5) {
+            return primes2[l - n / 5 - 1];
         }
-        else if(l <= 3*n/5){
-            return primes3[l-2*n/5-1];
+        else if (l <= 3 * n / 5) {
+            return primes3[l - 2 * n / 5 - 1];
         }
-        else if(l <= 4*n/5){
-            return primes4[l-3*n/5-1];
+        else if (l <= 4 * n / 5) {
+            return primes4[l - 3 * n / 5 - 1];
         }
-        else{
-            return primes5[l-4*n/5-1];
+        else {
+            return primes5[l - 4 * n / 5 - 1];
         }
     }
     console.log("filling array");
-    primes1 = Array(n/5 + 1).fill(true);  // Assume all numbers in array are prime
+    primes1 = Array(n / 5 + 1).fill(true);  // Assume all numbers in array are prime
     console.log("1 done");
-    primes2 = Array(n/5).fill(true);  // Assume all numbers in array are prime
+    primes2 = Array(n / 5).fill(true);  // Assume all numbers in array are prime
     console.log("2 done");
-    primes3 = Array(n/5).fill(true);  // Assume all numbers in array are prime
+    primes3 = Array(n / 5).fill(true);  // Assume all numbers in array are prime
     console.log("3 done");
-    primes4 = Array(n/5).fill(true);  // Assume all numbers in array are prime
+    primes4 = Array(n / 5).fill(true);  // Assume all numbers in array are prime
     console.log("4 done");
-    primes5 = Array(n/5).fill(true);  // Assume all numbers in array are prime
+    primes5 = Array(n / 5).fill(true);  // Assume all numbers in array are prime
     console.log("Array filling done!");
     primes1[0] = primes1[1] = false;         // Except for 0 and 1
     k = 0;
@@ -59,23 +59,23 @@ function sieveOfEratosthenes(n = 100) {
     for (let i = 2; i <= Math.sqrt(n); i++) {
         if (getValue(i)) {
             for (let j = i * i; j <= n; j += i) {
-                if (getValue(j)){k++;}         
-                if (j <= n/5){
+                if (getValue(j)) { k++; }
+                if (j <= n / 5) {
                     primes1[j] = false;
                 }
-                else if(j <= 2*n/5){
-                    primes2[j-n/5-1] = false;
+                else if (j <= 2 * n / 5) {
+                    primes2[j - n / 5 - 1] = false;
                 }
-                else if(j <= 3*n/5){
-                    primes3[j-2*n/5-1] = false;
+                else if (j <= 3 * n / 5) {
+                    primes3[j - 2 * n / 5 - 1] = false;
                 }
-                else if(j <= 4*n/5){
-                    primes4[j-3*n/5-1] = false;
+                else if (j <= 4 * n / 5) {
+                    primes4[j - 3 * n / 5 - 1] = false;
                 }
-                else{
-                    primes5[j-4*n/5-1] = false;
+                else {
+                    primes5[j - 4 * n / 5 - 1] = false;
                 }
-                if (k%20000 == 0){
+                if (k % 20000 == 0) {
                     process.stdout.write(`\r${k}`);
                 }
             }
@@ -90,22 +90,22 @@ function sieveOfEratosthenes(n = 100) {
     }, []));
     console.log("1 done");
     primes.push(primes2.reduce((acc, isPrime, index) => {
-        if (isPrime) acc.push(index + n/5 + 1);
+        if (isPrime) acc.push(index + n / 5 + 1);
         return acc;
     }, []));
     console.log("2 done");
     primes.push(primes3.reduce((acc, isPrime, index) => {
-        if (isPrime) acc.push(index + 2*n/5 + 1);
+        if (isPrime) acc.push(index + 2 * n / 5 + 1);
         return acc;
     }, []));
     console.log("3 done");
     primes.push(primes4.reduce((acc, isPrime, index) => {
-        if (isPrime) acc.push(index + 3*n/5 + 1);
+        if (isPrime) acc.push(index + 3 * n / 5 + 1);
         return acc;
     }, []));
     console.log("4 done");
     primes.push(primes5.reduce((acc, isPrime, index) => {
-        if (isPrime) acc.push(index + 4*n/5 + 1);
+        if (isPrime) acc.push(index + 4 * n / 5 + 1);
         return acc;
     }, []));
     console.log("5 done")
