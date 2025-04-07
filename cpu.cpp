1720ce78b10f346e3d2cf80d87ffde217fdc0c7c
RandomPerson238 <somebodyrandom92@outlook.com>
2025-04-07 16:29:20 -0700

    c++ things also autotools

---
+++ cpu.cpp
@@ -0,0 +1,22 @@
+#include<cstdlib>
+#include<unistd.h>
+#include<iostream>
+#include<string>
+#include<ncurses.h>
+#include<fstream>
+#include<sstream>
+using namespace std;
+
+int main(){
+    initscr();
+    while(true){
+        move(1,1);
+        system("ps -Opmem=PMEM,pcpu=PCPU,nice > temp.txt");
+        ifstream file("temp.txt");
+        ostringstream stream;
+        stream << file.rdbuf();
+        printw(stream.str().c_str());
+        refresh();
+        getch();
+    }
+}
--- main.cpp
+++ main.cpp
@@ -4,46 +4,44 @@
 #include<string>
 #include<ctime>
 #include<iomanip>
+#include<math.h>
+#include<cstdint>
 using namespace std;
 
 /*
     1. PRIORITY Next improve memory consumption in the store file part
     by popping list items whenever processed instead of at the end
     2. Also figure out how to use malloc (not important)
-*/ 
+*/
 
-void newfile(string filename, string data){
-    ofstream input(filename);
-    input << data;
-    input.close();
-}
 int main(int argc, char *argv[]){
     time_t start = time(NULL);
     cout << "start init" << endl;
-    int num;
+    unsigned long num;
     if(argc == 1){
         num = 100;
     }else{
-        num = stoi(argv[1]);
+        num = stoul(argv[1]);
     }
     vector<bool> primes(num + 1, true);
     primes[0] = false; primes[1] = false;
     cout << "end init" << endl;
-    for (int i = 2; i <= sqrt(num); i++) {
+    unsigned long limit = sqrt(num);
+    for (unsigned long i = 2; i <= limit; i++) {
         if (primes[i]) {
-            for (int j = i * i; j <= num; j += i) {
+            for (unsigned long j = i * i; j <= num; j += i) {
                 primes[j] = false;
             }
         }
     }
     cout << endl << "storing files" << endl;
-    string final("");
-    for(int i = 0; i<=primes.size(); i++){
+    ofstream file("primes.txt");
+    unsigned long size = primes.size();
+    for(unsigned long i = 0; i<=size; i++){
         if(primes[i]){
-            final += to_string(i) + string(",");
+            file << to_string(i) << " ";
         }
     }
-    newfile(string("primes.txt"), final);
     time_t finish = time(NULL);
     double time = double(finish) - double(start);
     cout << "it took " << setprecision(15) << time << " seconds." << endl;
